937345b64766e481b69366dfbc954d05d62dd6a1
francis560 <francispinales94@gmail.com>
2021-10-31 20:36:49 -0400

    Configuración basica creada

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="w-full bg-red-400">
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
